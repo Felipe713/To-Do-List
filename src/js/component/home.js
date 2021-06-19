25d0e939da2136a5c1cbea89dc86e5e0ee830661
Felipe713 <felipeivp@gmail.com>
2021-06-19 15:08:50 +0000

    to do list

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -5,27 +5,7 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
-	const [color, setColor] = useState("green");
 	return (
-		<div className="text-center mt-5">
-			<div id="trafficTop"></div>
-			<div id="container">
-				<div
-					className={`semaforo red ${
-						color === "red" ? "light" : ""
-					} `}
-					onClick={() => setColor("red")}></div>
-				<div
-					className={`semaforo yellow ${
-						color === "yellow" ? "light" : ""
-					} `}
-					onClick={() => setColor("yellow")}></div>
-				<div
-					className={` semaforo green ${
-						color === "green" ? "light" : ""
-					} `}
-					onClick={() => setColor("green")}></div>
-			</div>
-		</div>
+		
 	);
 }

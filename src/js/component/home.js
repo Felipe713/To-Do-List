b529d2ce4abbef6149ee7139cd02cdf0b04c63bc
Felipe713 <felipeivp@gmail.com>
2021-06-24 18:48:05 +0000

    To do list

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -3,9 +3,49 @@ import React, { useState } from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 export function Home() {
+	// 01 - Crear un array
+	const [todos, setTodos] = useState(["tarea 1", "tareas 2", "tarea 3"]);
+	/// 02 guardar texto que digito el usuario
+	const [task, setTask] = useState("");
 	return (
-		
+		// 03 Hacer el formulario
+		<div className="text-center mt-5">
+			<h1>TODO LIST</h1>
+			<span>{JSON.stringify(task)}</span>
+			<form
+				onSubmit={evento => {
+					evento.preventDefault();
+					setTodos([...todos, task]);
+					setTask("");
+				}}>
+				<input
+					placeholder="Ingrese su tarea"
+					onChange={evento => setTask(evento.target.value)}
+					value={task}></input>
+				<button>Agregar</button>
+			</form>
+			{/* 04 mostrar el contenido del arreglo */}
+			<ul>
+				{todos.map((item, index) => {
+					return (
+						<li key={index}>
+							<span>{item}</span>
+							<button
+								onClick={() =>
+									console.log(
+										"eliminar elemento",
+										index,
+										"contenido:",
+										todos[index]
+									)
+								}>
+								X
+							</button>
+						</li>
+					);
+				})}
+			</ul>
+		</div>
 	);
 }

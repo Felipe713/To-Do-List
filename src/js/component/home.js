1e647c5195a9e725eb9957507b56cfe44fdd0c0b
Felipe713 <felipeivp@gmail.com>
2021-06-25 00:24:13 +0000

    to do

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -5,14 +5,21 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 export function Home() {
 	// 01 - Crear un array
-	const [todos, setTodos] = useState(["tarea 1", "tareas 2", "tarea 3"]);
+	const [todos, setTodos] = useState([]);
 	/// 02 guardar texto que digito el usuario
 	const [task, setTask] = useState("");
+	function todosEliminar(index) {
+		console.log("funcion eliminar");
+		if (index > -1) {
+			const filterList = todos.filter(item => item !== todos[index]);
+			setTodos(filterList);
+		}
+	}
 	return (
 		// 03 Hacer el formulario
-		<div className="text-center mt-5">
+		<div className="text-center mt-5 container">
 			<h1>TODO LIST</h1>
-			<span>{JSON.stringify(task)}</span>
+			{/* <span>{JSON.stringify(task)}</span> */}
 			<form
 				onSubmit={evento => {
 					evento.preventDefault();
@@ -20,32 +27,28 @@ export function Home() {
 					setTask("");
 				}}>
 				<input
+					className="container"
 					placeholder="Ingrese su tarea"
 					onChange={evento => setTask(evento.target.value)}
 					value={task}></input>
 				<button>Agregar</button>
 			</form>
 			{/* 04 mostrar el contenido del arreglo */}
-			<ul>
+			<ul className="list-group">
 				{todos.map((item, index) => {
 					return (
-						<li key={index}>
+						<li className="list-group-item" key={index}>
 							<span>{item}</span>
-							<button
-								onClick={() =>
-									console.log(
-										"eliminar elemento",
-										index,
-										"contenido:",
-										todos[index]
-									)
-								}>
-								X
+							<button onClick={() => todosEliminar(index)}>
+								<i class="fa fa-trash" aria-hidden="true"></i>
 							</button>
 						</li>
 					);
 				})}
 			</ul>
+			<div>
+				<p>{todos.length} Items left</p>
+			</div>
 		</div>
 	);
 }
